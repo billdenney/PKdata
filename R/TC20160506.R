a48b64726220ee92cc6f198cff8f16a5145a0b31
Bill Denney <wdenney@humanpredictions.com>
2020-01-01 15:47:49 -0500

    Updates for TC20160506

--- R/TC20160506.R
+++ R/TC20160506.R
@@ -1,50 +1,52 @@
-#' Pharmacokinetics of TC20160506
+#' Pharmacokinetics of TC20160506 (Note that TC20160506 is a de-identified name for the drug.)
 #' 
-#' De-identified data set of concentration-time data for TC20160506.
-#' (Note that TC20160506 is a de-identified name for the drug.)  When
-#' working with this dataset, be aware that the \code{ANALYTE} named
-#' "Parent" can be converted into "Metabolite".
+#' De-identified data set of concentration-time data for TC20160506. When
+#' working with this dataset, be aware that the \code{ANALYTE} named "Parent"
+#' can be converted into "Metabolite".
 #' 
-#' @format A data frame with four variables:
+#' @format \code{TC20160506_conc} is concentration-time data.  The data.frame
+#'   has the following variables:
 #' \describe{
 #'   \item{\code{ID}}{Subject identifier}
 #'   \item{\code{ANALYTE}}{Analyte being measured}
 #'   \item{\code{TIME}}{Time since first dose on \code{DAY} 1 (in hours)}
+#'   \item{\code{TIMEU}}{Units for time, "hours"}
 #'   \item{\code{CONC}}{Plasma concentration of TC20160506 (in ug/L)}
+#'   \item{\code{CONCU}}{Units for plasma concentration of TC20160506, "ug/L"}
 #' }
-#' @seealso \code{\link{TC20160506.dose}}, \code{\link{TC20160506.demog}}
-"TC20160506.conc"
-
-#' Dosing of TC20160506
 #' 
-#' De-identified data set of dosing data for TC20160506.  (Note that
-#' TC20160506 is a de-identified name for the drug.)
-#' 
-#' @format A data frame with five variables:
+#' @format \code{TC20160506_dose} is dosing data for TC20160506.  The data.frame
+#'   has the following variables:
 #' \describe{
 #'   \item{\code{ID}}{Subject identifier}
 #'   \item{\code{ANALYTE}}{Analyte being dosed (the drug name)}
 #'   \item{\code{TIME}}{Time since first dose on \code{DAY} 1 (in
 #'     hours)}
+#'   \item{\code{TIMEU}}{Units for time, "hours"}
 #'   \item{\code{DOSE}}{Dose of TC20160506 (in ug).  Note that there
 #'     may be more than one dose record per subject.}
+#'   \item{\code{DOSEU}}{Units for dose, "ug"}
 #'   \item{\code{ROUTE}}{Route of administration}
 #' }
-#' @seealso \code{\link{TC20160506.conc}},
-#'   \code{\link{TC20160506.demog}}
-"TC20160506.dose"
-
-#' Demographics associated with the TC20160506 datasets
 #' 
-#' De-identified data set of demographic data for TC20160506 pharmacokinetics. 
-#' (Note that TC20160506 is a de-identified name for the drug.)
-#' 
-#' @format A data frame with three variables:
+#' @format \code{TC20160506_demog} is demographic data for TC20160506
+#'   pharmacokinetics.  The data.frame has the following variables:
 #' \describe{
 #'   \item{\code{ID}}{Subject identifier}
 #'   \item{\code{AGE}}{Subject's age (in years)}
+#'   \item{\code{AGEU}}{Units for age, "years"}
 #'   \item{\code{SEX}}{Subject's sex (Male/Female)}
 #'   \item{\code{WEIGHT}}{Subject's body weight (in kg)}
+#'   \item{\code{WEIGHTU}}{Units for body weight, "kg"}
 #' }
-#' @seealso \code{\link{TC20160506.conc}}, \code{\link{TC20160506.dose}}
-"TC20160506.demog"
+#' @name TC20160506
+NULL
+
+#' @rdname TC20160506
+"TC20160506_conc"
+
+#' @rdname TC20160506
+"TC20160506_dose"
+
+#' @rdname TC20160506
+"TC20160506_demog"
--- data-raw/TC20160506_ParentMetaboliteDataRich.R
+++ data-raw/TC20160506_ParentMetaboliteDataRich.R
@@ -4,45 +4,72 @@
 ## identifiers blinded.
 
 library(devtools)
-library(dplyr)
+library(tidyverse)
+library(assertr)
 
 local({
-  tmpdata <- read.csv("data-raw/TC20160506_ParentMetaboliteDataRich.csv", stringsAsFactors=FALSE)
+  d_raw <- read.csv("data-raw/TC20160506_ParentMetaboliteDataRich.csv", stringsAsFactors=FALSE)
   
   ## Standardize column names
-  tmpdata <- rename(tmpdata,
-                    ANALYTE=Compound,
-                    ID=Subject,
-                    AGE=Age,
-                    WEIGHT=WGT,
-                    SEX=Gender,
-                    DOSE.LEVEL=DoseLevel,
-                    DOSE.TOTAL=TotalDose,
-                    DOSE=AMT,
-                    TIME=Time,
-                    CONC=Conc)
-  tmpdata$ID <- factor(tmpdata$ID)
+  d_raw <-
+    d_raw %>%
+    rename(
+      ANALYTE=Compound,
+      ID=Subject,
+      AGE=Age,
+      WEIGHT=WGT,
+      SEX=Gender,
+      DOSE.LEVEL=DoseLevel,
+      DOSE.TOTAL=TotalDose,
+      DOSE=AMT,
+      TIME=Time,
+      CONC=Conc
+    ) %>%
+    mutate(
+      ID=factor(ID)
+    )
 
   ## Create the demographics  
-  TC20160506.demog <- unique(tmpdata[,c("ID", "AGE", "WEIGHT", "SEX")])
-  if (length(unique(TC20160506.demog$ID)) != nrow(TC20160506.demog))
-    stop("Error generating TC20160506 demographics")
-  
-  ## Create the dosing
-  TC20160506.dose <-
-    unique(tmpdata[!(tmpdata$DOSE %in% 0),
-                   c("ID", "ANALYTE", "TIME", "DOSE")])
-  TC20160506.dose$ROUTE <- "Intranasal"
+  TC20160506_demog <-
+    d_raw %>%
+    select("ID", "AGE", "WEIGHT", "SEX") %>%
+    unique() %>%
+    mutate(
+      AGEU="years",
+      WEIGHTU="kg"
+    ) %>%
+    verify(!duplicated(ID))
 
-  TC20160506.conc <- tmpdata[,c("ID", "ANALYTE", "TIME", "CONC")]
-  ## Set BLQ data to 0
-  TC20160506.conc$CONC[is.na(TC20160506.conc$CONC)] <- 0
-  TC20160506.conc <- unique(TC20160506.conc)
+  ## Create the dosing
+  TC20160506_dose <-
+    d_raw %>%
+    filter(!(DOSE %in% 0)) %>%
+    select("ID", "ANALYTE", "TIME", "DOSE") %>%
+    mutate(
+      DOSEU="ug",
+      TIMEU="hours",
+      ROUTE="Intranasal"
+    )
+
+  TC20160506_conc <-
+    d_raw %>%
+    select("ID", "ANALYTE", "TIME", "CONC") %>%
+    mutate(
+      CONC=
+        case_when(
+          is.na(CONC)~0,
+          TRUE~CONC
+        ),
+      CONCU="ug/L",
+      TIMEU="hours"
+    ) %>%
+    unique()
 
   # Save the data
-  devtools::use_data(TC20160506.demog,
-                     TC20160506.dose,
-                     TC20160506.conc,
-                     pkg=".",
-                     overwrite=TRUE)
+  usethis::use_data(
+    TC20160506_demog,
+    TC20160506_dose,
+    TC20160506_conc,
+    overwrite=TRUE
+  )
 })
