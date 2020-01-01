57f960548b113dfc3b563047bb756d7b2dba5236
Bill Denney <wdenney@humanpredictions.com>
2020-01-01 13:03:05 -0500

    Clean up CP1805

--- R/CP1805.R
+++ R/CP1805.R
@@ -1,44 +1,48 @@
 #' Pharmacokinetics of CP1805
 #' 
-#' De-identified data set of concentration-time data for CP1805.
-#' 
-#' @format A data frame with five variables:
+#' @format \code{CP1805_conc} is a de-identified data set of concentration-time
+#'   data for CP1805 as a data frame with the following variables:
 #' \describe{
 #'   \item{\code{ID}}{Subject identifier}
 #'   \item{\code{DAY}}{Day of measurement}
-#'   \item{\code{TIME.AFTER.DOSE}}{Time since the dose on the current \code{DAY} (in hours)}
-#'   \item{\code{CONC}}{Plasma concentration of CP1805 (in ug/mL)}
+#'   \item{\code{TIME_AFTER_DOSE}}{Time since the dose on the current \code{DAY} (in hours)}
 #'   \item{\code{TIME}}{Time since first dose on \code{DAY} 1 (in hours)}
+#'   \item{\code{TIMEU}}{Units for time, "hours"}
+#'   \item{\code{CONC}}{Plasma concentration of CP1805 (in ug/mL)}
+#'   \item{\code{CONCU}}{Units for plasma concentration of CP1805, "ug/mL"}
 #' }
-#' @seealso \code{\link{CP1805.dose}}, \code{\link{CP1805.demog}}
-"CP1805.conc"
-
-#' Dosing of CP1805
 #' 
-#' De-identified data set of dosing data for CP1805.
-#' 
-#' @format A data frame with five variables:
+#' @format \code{CP1805_dose} is a de-identified data set of dosing data for
+#'   CP1805 as a data frame with the following variables:
 #' \describe{
 #'   \item{\code{ID}}{Subject identifier}
 #'   \item{\code{ROUTE}}{Route of administration}
 #'   \item{\code{DAY}}{Day of measurement}
-#'   \item{\code{DOSE}}{Total dose of CP1805 (in mg)}
-#'   \item{\code{DOSE.WEIGHT}}{Dose of CP1805 normalized by body weight (in mg/kg)}
-#'   \item{\code{TIME.AFTER.DOSE}}{Time since the dose on the current \code{DAY} (in hours)}
+#'   \item{\code{TIME_AFTER_DOSE}}{Time since the dose on the current \code{DAY} (in hours)}
 #'   \item{\code{TIME}}{Time since first dose on \code{DAY} 1 (in hours)}
+#'   \item{\code{TIMEU}}{Units for time, "hours"}
+#'   \item{\code{DOSE}}{Total dose of CP1805 (in mg)}
+#'   \item{\code{DOSEU}}{Total dose of CP1805, "mg"}
+#'   \item{\code{NDOSE}}{Dose of CP1805 normalized by body weight (in mg/kg)}
+#'   \item{\code{NDOSEU}}{Units for dose of CP1805 normalized by body weight, "mg/kg"}
 #' }
-#' @seealso \code{\link{CP1805.conc}}, \code{\link{CP1805.demog}}
-"CP1805.dose"
-
-#' Demographics associated with the CP1805 datasets
 #' 
-#' De-identified data set of demographic data for CP1805 pharmacokinetics.
-#' 
-#' @format A data frame with three variables:
+#' @format \code{CP1805_demog} is a de-identified data set of demographic data
+#'   for CP1805 pharmacokinetics as a data frame with the following variables:
 #' \describe{
 #'   \item{\code{ID}}{Subject identifier}
 #'   \item{\code{SEX}}{Subject's sex (Male/Female)}
-#'   \item{\code{WEIGHT}}{Subject's body weight (in kg)}
+#'   \item{\code{WEIGHT}}{Subject's body weight (kg)}
+#'   \item{\code{WEIGHTU}}{Units for subject's body weight, "kg"}
 #' }
-#' @seealso \code{\link{CP1805.conc}}, \code{\link{CP1805.dose}}
-"CP1805.demog"
+#' @name CP1805
+NULL
+
+#' @rdname CP1805
+"CP1805_conc"
+
+#' @rdname CP1805
+"CP1805_dose"
+
+#' @rdname CP1805
+"CP1805_demog"
--- data-raw/CP1805Data.R
+++ data-raw/CP1805Data.R
@@ -8,16 +8,22 @@ library(dplyr)
 
 local({
   ## Read the header
-  headernames <- unlist(read.csv("data-raw/CP1805Data.csv",
-                                 header=FALSE,
-                                 nrow=1,
-                                 stringsAsFactors=FALSE))
+  headernames <-
+    unlist(read.csv(
+      "data-raw/CP1805Data.csv",
+      header=FALSE,
+      nrow=1,
+      stringsAsFactors=FALSE
+    ))
   ## Skip the second row of the file and read the rest of the file
-  tmpdata <- read.csv("data-raw/CP1805Data.csv",
-                      header=FALSE,
-                      skip=2,
-                      col.names=headernames,
-                      stringsAsFactors=FALSE)
+  tmpdata <-
+    read.csv(
+      "data-raw/CP1805Data.csv",
+      header=FALSE,
+      skip=2,
+      col.names=headernames,
+      stringsAsFactors=FALSE
+    )
   ## Standardize naming
   tmpdata$ID <- factor(tmpdata$Subject)
   tmpdata$SEX <- factor(tmpdata$Gender)
@@ -25,26 +31,45 @@ local({
   tmpdata$Day <- NULL
 
   ## Separate out the demographic data
-  CP1805.demog <- rename(unique(tmpdata[,c("ID", "SEX", "WGTkg")]),
-                         WEIGHT=WGTkg)
+  CP1805_demog <-
+    tmpdata %>%
+    select(ID, SEX, WEIGHT=WGTkg) %>%
+    unique() %>%
+    mutate(WEIGHTU="kg")
   
   ## Separate out the dosing data
-  CP1805.dose <- rename(unique(tmpdata[,c("ID", "Route", "DAY", "Dose", "Dosemgkg")]),
-                        ROUTE=Route,
-                        DOSE=Dose,
-                        DOSE.WEIGHT=Dosemgkg)
-  CP1805.dose$TIME.AFTER.DOSE <- 0
-  CP1805.dose$TIME <- (CP1805.dose$DAY - 1)*24 + CP1805.dose$TIME.AFTER.DOSE
+  CP1805_dose <-
+    tmpdata %>%
+    select(ID, ROUTE="Route", "DAY", DOSE="Dose", NDOSE="Dosemgkg") %>%
+    mutate(
+      TIME_AFTER_DOSE=0,
+      TIME=(DAY - 1)*24 + TIME_AFTER_DOSE,
+      TIMEU="hours",
+      DOSEU="mg",
+      NDOSEU="mg/kg"
+    ) %>%
+    select(
+      ID, ROUTE,
+      DAY, TIME_AFTER_DOSE, TIME, TIMEU,
+      DOSE, DOSEU,
+      NDOSE, NDOSEU,
+      everything()
+    )
 
-  CP1805.conc <- rename(tmpdata[,c("ID", "DAY", "Time", "Conc")],
-                        CONC=Conc,
-                        TIME.AFTER.DOSE=Time)
-  CP1805.conc$TIME <- (CP1805.conc$DAY - 1)*24 + CP1805.conc$TIME.AFTER.DOSE
+  CP1805_conc <-
+    tmpdata %>%
+    select(ID, DAY, TIME_AFTER_DOSE=Time, CONC=Conc) %>%
+    mutate(
+      TIME=(DAY - 1)*24 + TIME_AFTER_DOSE,
+      TIMEU="hours",
+      CONCU="ug/mL"
+    )
 
   # Save the data
-  devtools::use_data(CP1805.demog,
-                     CP1805.dose,
-                     CP1805.conc,
-                     pkg=".",
-                     overwrite=TRUE)
+  usethis::use_data(
+    CP1805_conc,
+    CP1805_demog,
+    CP1805_dose,
+    overwrite=TRUE
+  )
 })
